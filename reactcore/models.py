3f62ee08d1f38d858013b19080019c5d971a31e5
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-10-21 23:44:51 +0530

    first commit

---
+++ reactcore/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+# Create your models here.
+class Books(models.Model):
+    book_name = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.FloatField()
+    publication = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.book_name
+    
+class Students(models.Model):
+    student_name = models.CharField(max_length=100)
+    user_ID = models.CharField(max_length=100)
+    institution = models.CharField(max_length=100)
+    book_chosen = models.ForeignKey(Books, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.student
