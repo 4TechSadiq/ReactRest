a347c8eb65b78fa6d7c24900861ef50449f8576a
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-10-23 18:37:24 +0530

    Refactor urls.py to add CRUD routes for books and students

--- reactcore/urls.py
+++ reactcore/urls.py
@@ -2,7 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CreateBook.as_view(), name="create-book"),
-    path("list", views.ListBooks.as_view(), name="list-books"),
+    path("create-book", views.CreateBook.as_view(), name="create-book"),
+    path("update-book/<int:pk>", views.UpdateBook.as_view(), name="update-book"),
+    path("list-book", views.ListBooks.as_view(), name="list-books"),
+    path("delete-book", views.DeleteBook.as_view(), name="delete-book"),
     path("create-student", views.CreateStudent.as_view(), name="create-student"),
+    path("update-student/<int:pk>", views.UpdateStudent.as_view(), name="update-student"),
+    path("list-student", views.ListStudent.as_view(), name="list-student"),
+    path("delete-student", views.DeleteStudent.as_view(), name="delete-student"),
+
 ]
