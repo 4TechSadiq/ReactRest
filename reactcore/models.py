bf51e1162a3f5304d3411c3a5dbc94ebcd83eac8
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-10-23 18:32:14 +0530

    Refactor models.py to use ManyToManyField for book_chosen in Students model

--- reactcore/models.py
+++ reactcore/models.py
@@ -15,7 +15,7 @@ class Students(models.Model):
     student_name = models.CharField(max_length=100)
     user_ID = models.CharField(max_length=100)
     institution = models.CharField(max_length=100)
-    book_chosen = models.ForeignKey(Books, on_delete=models.CASCADE)
+    book_chosen = models.ManyToManyField(Books)
 
     def __str__(self):
         return self.student
@@ -23,7 +23,7 @@ class Students(models.Model):
 class Catelog(models.Model):
     catelog_name = models.CharField(max_length=500)
     catelog_desc = models.CharField(max_length=500)
-    catelog_books = models.CharField(max_length=500)
+    catelog_books = models.ManyToManyField(Books)
     catelog_image = models.ImageField(upload_to='images/')
 
     def __str__(self):
--- reactcore/views.py
+++ reactcore/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework import generics
-from .models import Books
+from .models import Books,Students
 from .serializers import BooksSerializer,StudentsSerializer
+from rest_framework.permissions import AllowAny
 # Create your views here.
 
 class CreateBook(generics.ListCreateAPIView):
@@ -12,6 +13,20 @@ class ListBooks(generics.ListAPIView):
     queryset = Books.objects.all()
     serializer_class = BooksSerializer
 
-class CreateStudent(generics.ListCreateAPIView):
+class UpdateBook(generics.UpdateAPIView):
     queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+    permission_classes = [AllowAny]
+
+class DeleteBook(generics.DestroyAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+    permission_classes = [AllowAny]
+    
+
+
+
+
+class CreateStudent(generics.ListCreateAPIView):
+    queryset = Students.objects.all()
     serializer_class = StudentsSerializer
