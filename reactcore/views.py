d3be051aaa56a6d289bfdfd2730bfa18899ba652
Mohammed Sadiq Ali <sadikcp2014@gmail.com>
2024-10-23 12:47:37 +0530

    Refactor views.py to add BooksList view for listing and creating books

--- reactcore/views.py
+++ reactcore/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
+from .models import Books
+from .serializers import BooksSerializer,StudentsSerializer
 # Create your views here.
+
+class BooksList(generics.ListCreateAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
